96a8d8860763d721dca53c17d99e6467a47fd47a
Takeshi Takatsudo <takazudo@gmail.com>
2020-03-24 22:06:51 +0900

    bump

---
+++ pages/3.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+const Home = () => (
+  <div className="container">
+    <main>
+      <h1>3.js</h1>
+      <ul>
+        <li><Link href="/"><a>back to index (Link)</a></Link></li>
+        <li><a href="/">back to index (just an anchor)</a></li>
+      </ul>
+    </main>
+  </div>
+)
+
+export default Home
